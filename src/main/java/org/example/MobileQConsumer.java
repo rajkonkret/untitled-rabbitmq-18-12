a881ab857ae8d6cc7aa100e7088b1ad4c737514e
rajkonkret <rajkonkret660@gmail.com>
2023-12-18 14:59:05 +0100

    add AcqConsumer

--- src/main/java/org/example/MobileQConsumer.java
+++ src/main/java/org/example/MobileQConsumer.java
@@ -5,19 +5,20 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class MessageSubscriber {
+public class MobileQConsumer {
     public static void main(String[] args) throws IOException, TimeoutException {
-
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.newConnection(CommonConfig.AMQP_URL);
         Channel channel = connection.createChannel();
 
-        DeliverCallback deliverCallback = (s, delivery) -> {
-            System.out.println(new String(delivery.getBody(), "UTF-8"));
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            System.out.println(consumerTag);
+            System.out.println(new String(message.getBody(), "UTF-8"));
         };
-        CancelCallback cancelCallback = s -> {
-            System.out.println(s);
+        CancelCallback cancelCallback = consumerTag -> {
+            System.out.println(consumerTag);
         };
-        channel.basicConsume(CommonConfig.DEAFAULT_QUEUE, true, deliverCallback, cancelCallback);
+        channel.basicConsume("MobileQ", true, deliverCallback, cancelCallback);
+
     }
 }
