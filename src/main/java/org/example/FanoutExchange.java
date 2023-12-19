3796445128a56dfa15891996b3afdfab131ec1f2
rajkonkret <rajkonkret660@gmail.com>
2023-12-19 08:56:37 +0100

    update Fanout exchange

--- src/main/java/org/example/FanoutExchange.java
+++ src/main/java/org/example/FanoutExchange.java
@@ -23,4 +23,40 @@ public class FanoutExchange {
 
         chanel.close();
     }
+
+    public static void declareBindings() throws IOException, TimeoutException {
+        Channel channel = ConnectionManager.getConnection().createChannel();
+
+        // Tworzenie linkow fanout kolejke, exchange, routingKey != null
+        channel.queueBind("MobileQ", "my-fanout-exchange", "");
+        channel.queueBind("ACQ", "my-fanout-exchange", "");
+        channel.queueBind("LightQ", "my-fanout-exchange", "");
+
+        channel.close();
+    }
+
+    public static void subscribeManager() throws IOException, TimeoutException {
+        Channel channel = ConnectionManager.getConnection().createChannel();
+
+        channel.basicConsume("LightQ", true, ((consumerTag, message) -> {
+            System.out.println(consumerTag);
+            System.out.println("LightQ" + new String(message.getBody()));
+        }), consumerTag -> {
+            System.out.println(consumerTag);
+        });
+
+        channel.basicConsume("ACQ", true, ((consumerTag, message) -> {
+            System.out.println(consumerTag);
+            System.out.println("ACQ" + new String(message.getBody()));
+        }), consumerTag -> {
+            System.out.println(consumerTag);
+        });
+
+        channel.basicConsume("MobileQ", true, ((consumerTag, message) -> {
+            System.out.println(consumerTag);
+            System.out.println("MobileQ" + new String(message.getBody()));
+        }), consumerTag -> {
+            System.out.println(consumerTag);
+        });
+    }
 }
